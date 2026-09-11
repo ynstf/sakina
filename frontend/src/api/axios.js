89d89e2ab1ca67dbe1d1583946ec761e82b6ba34
Youness <younessatif2000@gmail.com>
2026-09-11 19:50:13 +0100

    change the api to the production in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'http://84.8.219.196:8000/api/',
     headers: {
         'Content-Type': 'application/json',
     },
